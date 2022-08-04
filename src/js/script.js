01de7a1ef542521ba0c9910a826ec9ebcb1412fa
netomilfont <netomilfont@hotmail.com>
2022-08-04 01:50:20 -0300

    deixando responsivo e mobile first

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,25 @@
 let buttonNavbar = document.querySelector("#botoesContainer")
-let ulProducts = document.querySelector("ul")
+let ulProducts = document.querySelector(".productsList")
+let ulCarProduct = document.querySelector(".carProduct")
+let cartShopping = []
 
-function listCards (array) {
+function listCards (array, section) {
     
-    ulProducts.innerHTML = ""
-    
+    section.innerHTML = ''
+
     array.forEach((element) => {
 
         let product = element
         
         let cardProduct = createCardProduct(product)
         
-        ulProducts.appendChild(cardProduct)
-        
-        createCardProduct(product)
-        addingPrice(array)
+        section.appendChild(cardProduct)
 
     })
     
 }
 
-listCards(produtos)
+listCards(produtos, ulProducts)
 
 function searchSpecificCard (array, section) {
 
@@ -55,26 +54,52 @@ buttonNavbar.addEventListener("click", (event) => {
         listCards(specificCard)
 
     } else if(btn.tagName == "BUTTON" && btn.innerText == 'Todos Produtos') {
-
+        
         listCards(produtos)
     }
 })
 
 function createCardProduct (array) {
 
+    let id = array.id
+
     let ul = document.querySelector("ul")
     let li = document.createElement("li")
+    let divImg = document.createElement("div")
     let img = document.createElement("img")
+    let div1 = document.createElement("div")
+    let div = document.createElement("div")
     let h3 = document.createElement("h3")
     let span = document.createElement("span")
+    let ol = document.createElement("ol")
+    let li2 = document.createElement("li")
     let p = document.createElement("p")
+    let div3 = document.createElement("div")
+    let button = document.createElement("button")
+
+    if(id != undefined){
+
+        button.id =  id
+    }
 
     img.src = array.img
     h3.innerText = array.nome
     span.innerText = array.categoria
-    p.innerText = `R$ ${array.preco},00`
-
-    li.append(img, h3, span, p)
+    p.innerText = `R$ ${array.preco}`
+    li2.innerText = array.componentes
+    button.innerText = 'Comprar'
+    button.classList.add("buyButton")
+    li2.classList.add("hiddenComponentes")
+    div1.classList.add("infoProduct")
+    div.classList.add("titleAndPrice")
+    div3.classList.add("divButton")
+    
+    div3.append(button)
+    ol.append(li2)
+    div.append(h3, span, ol, p)
+    div1.append(div, div3)
+    divImg.append(img)
+    li.append(divImg, div1)
     
     return li
 
@@ -101,36 +126,92 @@ function searchProduct (searchValue) {
 
     let resultSearch = produtos.filter((element) => {
 
-        if(searchValue == element.nome) {
+        if(searchValue == element.nome || searchValue == element.categoria || searchValue == element.secao) {
 
             return element
         } else if(searchValue == '') {
 
             return element
         }
-
-    
-        
     })
 
     return resultSearch
 
 }
 
-function addingPrice (array) {
-    
-    let spanTotal = document.querySelector(".price")
+function createCartTotal (cart) {
 
-    spanTotal.innerText = sum(array)
+    let span1 = document.querySelector(".quantity")
+    let span2 = document.querySelector(".total")
     
+    span1.innerText = cart.length
+    span2.innerText = sum(cart)
+
 }
 
 function sum (array) {
     
     let priceTotal = array.reduce((previousValue, currentValue) => {
 
-        return previousValue += currentValue.preco
+        return previousValue += Number(currentValue.preco)
     }, 0)
 
     return `R$ ${priceTotal},00`
+
 }
+
+ulProducts.addEventListener("click",(event) => {
+    
+    let btnBuy = event.target
+    
+    if(btnBuy.tagName == "BUTTON") {
+    
+        let idProduct = btnBuy.id
+    
+        let product = produtos.find(function(product) {
+            if(product.id == idProduct) {
+                
+                return product
+            }
+        })
+        addCart(product)
+    }
+})
+
+function addCart (product) {
+
+    if(product !== undefined) {
+
+        cartShopping.push(product)
+
+        listCards(cartShopping, ulCarProduct)
+        createCartTotal(cartShopping)
+    }
+    
+}
+
+ulCarProduct.addEventListener("click", (event) => {
+
+    let btnRemove = event.target
+
+    if(btnRemove.tagName == 'BUTTON') {
+
+        let id = btnRemove.id
+
+        let index = cartShopping.findIndex((produto) => produto.id == id)
+        
+        cartShopping.splice(index, 1)
+
+        createCartTotal(cartShopping)
+        listCards(cartShopping, ulCarProduct)
+    }
+
+})
+
+// function createCartElement () {
+
+//     let ul = document.querySelector(".carProduct")
+//     let li = document.createElement("li")
+//     let 
+
+// }
